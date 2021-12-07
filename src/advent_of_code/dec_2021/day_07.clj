800aa7ba2578b62d69ed8c21b2579858af41fa1a
Tomas Ekholm <tomas.ekholm@tietoevry.com>
2021-12-07 13:33:10 +0100

    Added day 7

---
+++ src/advent_of_code/dec_2021/day_07.clj
@@ -0,0 +1,57 @@
+(ns advent-of-code.dec-2021.day-07
+  (:require [ysera.test :refer [is is-not is= deftest]]))
+
+(def test-input "16,1,2,0,4,2,7,1,2,14")
+
+(def puzzle-input (slurp "src/advent_of_code/dec_2021/day_07_input.txt"))
+
+(defn distance-fn
+  [x y]
+  (Math/abs (- x y)))
+
+(defn calculate-fuel
+  [d]
+  (reduce + (range 1 (inc d))))
+
+(def calculate-fuel (memoize calculate-fuel))
+
+(defn distance-2-fn
+  {:test (fn []
+           (is= (distance-2-fn 0 1) 1)
+           (is= (distance-2-fn 0 2) 3)
+           (is= (distance-2-fn 0 3) 6)
+           (is= (distance-2-fn 0 4) 10))}
+  [x y]
+  (let [original-distance (distance-fn x y)]
+    (calculate-fuel original-distance)))
+
+(defn align-positions
+  {:test (fn []
+           (is= (align-positions test-input distance-fn)
+                [2 37])
+           (is= (align-positions test-input distance-2-fn)
+                [5 168]))}
+  [input distance-fn]
+  (let [positions (->> (clojure.string/split input #",")
+                       (map read-string))]
+    (->> (range)
+         (map (fn [r]
+                (->> positions
+                     (map (fn [p] (distance-fn p r)))
+                     (reduce +))))
+         (partition 2 1)
+         (map-indexed (fn [index item] [index item]))
+         (some (fn [[index [p1 p2]]] (when (< p1 p2) [index p1]))))))
+
+
+(comment
+  (is= (time (align-positions puzzle-input distance-fn))
+       ; "Elapsed time: 1068.78478 msecs"
+       [349 356992]))
+
+(comment
+  (is= (time (align-positions puzzle-input distance-2-fn))
+       ; "Elapsed time: 3386.525335 msecs" without memoize
+       ; "Elapsed time: 1323.227414 msecs" with memoize
+       [489 101268110]))
+
