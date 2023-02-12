e8f4323c2242276b5df895a9680a1ea55969f14a
Tomas Ekholm <ekholm.tomas@gmail.com>
2023-02-12 12:52:14 +0100

    Completed 2022 calender

---
+++ src/advent_of_code/dec_2022/day_09.clj
@@ -0,0 +1,116 @@
+(ns advent-of-code.dec-2022.day-09
+  (:require [ysera.test :refer [is= is is-not]]))
+
+(def input (->> (slurp "src/advent_of_code/dec_2022/day_09_input.txt")
+                (clojure.string/split-lines)))
+
+(def test-input ["R 4" "U 4" "L 3" "D 1" "R 4" "D 1" "L 5" "R 2"])
+
+(def directions {"L" [-1 0]
+                 "D" [0 -1]
+                 "R" [1 0]
+                 "U" [0 1]})
+
+(def state {:head    [0 0]
+            :tail    [0 0]
+            :visited #{[0 0]}})
+
+(defn get-instructions
+  [input]
+  (->> input
+       (map (fn [instruction]
+              (let [result (clojure.string/split instruction #" ")]
+                {:direction (directions (first result))
+                 :amount    (read-string (second result))})))))
+
+(defn move-towards-head
+  {:test (fn []
+           (is= (move-towards-head [4 4] [2 2])
+                [3 3])
+           (is= (move-towards-head [2 0] [0 0])
+                [1 0]))}
+  [[hx hy] [tx ty :as tail]]
+  (let [dx (- hx tx)
+        dy (- hy ty)]
+    (if (<= (max (abs dx) (abs dy)) 1)
+      tail
+      (let [mx (cond (pos? dx) 1 (neg? dx) -1 :else 0)
+            my (cond (pos? dy) 1 (neg? dy) -1 :else 0)]
+        [(+ tx mx) (+ ty my)]))))
+
+(defn move
+  {:test (fn []
+           (is= (-> (move state test-input)
+                    (:visited)
+                    (count))
+                13))}
+  [state input]
+  (->> (reduce (fn [state {d :direction a :amount}]
+                 (reduce (fn [state _]
+                           (let [head-next-position (map + (:head state) d)
+                                 tail-next-position (move-towards-head head-next-position (:tail state))]
+                             (-> state
+                                 (assoc :head head-next-position)
+                                 (assoc :tail tail-next-position)
+                                 (update :visited conj tail-next-position))))
+                         state
+                         (range a)))
+               state
+               (get-instructions input))))
+
+(comment
+  (time (-> (move state input)
+            (:visited)
+            (count)))
+  ; "Elapsed time: 24.149976 msecs"
+  ; => 6311
+  )
+
+(def larger-input ["R 5" "U 8" "L 8" "D 3" "R 17" "D 10" "L 25" "U 20"])
+
+(def state2 {:rope    (vec (repeat 10 [0 0]))
+             :visited #{[0 0]}})
+
+(defn move-with-tail
+  {:test (fn []
+           (is= (-> (move-with-tail state2 test-input)
+                    (:visited)
+                    (count))
+                1)
+           (is= (-> (move-with-tail state2 larger-input)
+                    (:visited)
+                    (count))
+                36)
+           )}
+  [state input]
+  (->> (reduce (fn [state {d :direction a :amount}]
+                 (reduce (fn [state _]
+                           (let [head-next-position (map + (first (:rope state)) d)]
+                             (->> (:rope state)
+                                  (map-indexed (fn [index item] [index item]))
+                                  (drop 1)
+                                  ; index will now start from 1
+                                  (reduce (fn [state [index p]]
+                                            (let [local-head (get (:rope state) (dec index))
+                                                  new-p (move-towards-head local-head p)]
+                                              (-> (if (= index 9)
+                                                    (update state :visited conj new-p)
+                                                    state)
+                                                  (assoc-in [:rope index] new-p))))
+                                          (assoc-in state [:rope 0] head-next-position)))))
+                         state
+                         (range a)))
+               state
+               (get-instructions input))))
+
+(comment
+  (time (-> (move-with-tail state2 input)
+            (:visited)
+            (count)))
+  ; "Elapsed time: 86.682643 msecs"
+  ; => 2482
+  )
+
+
+
+
---
+++ src/advent_of_code/dec_2022/day_13.clj
@@ -0,0 +1,119 @@
+(ns advent-of-code.dec-2022.day-13
+  (:require [clojure.test :refer [deftest]]
+            [clojure.test :refer [deftest]]
+            [ysera.test :refer [is= is is-not]]))
+
+(def input (->> (slurp "src/advent_of_code/dec_2022/day_13_input.txt")
+                (clojure.string/split-lines)
+                (remove (fn [l] (= l "")))
+                (map read-string)
+                (partition 2)))
+
+(def test-input (->> "[1,1,3,1,1]\n[1,1,5,1,1]\n\n[[1],[2,3,4]]\n[[1],4]\n\n[9]\n[[8,7,6]]\n\n[[4,4],4,4]\n[[4,4],4,4,4]\n\n[7,7,7,7]\n[7,7,7]\n\n[]\n[3]\n\n[[[]]]\n[[]]\n\n[1,[2,[3,[4,[5,6,7]]]],8,9]\n[1,[2,[3,[4,[5,6,0]]]],8,9]"
+                     (clojure.string/split-lines)
+                     (remove (fn [l] (= l "")))
+                     (map read-string)
+                     (partition 2)))
+
+(defn correct-order
+  "Returns -1 if correct order, 0 if equal, 1 if incorrect order"
+  {:test (fn []
+           (is= (correct-order [1, 1, 3, 1, 1] [1, 1, 5, 1, 1])
+                -1)
+           (is= (correct-order [[1], [2, 3, 4]] [[1], 4])
+                -1)
+           (is= (correct-order [9] [[8, 7, 6]])
+                1)
+           (is= (correct-order [[4, 4], 4, 4] [[4, 4], 4, 4, 4])
+                -1)
+           (is= (correct-order [7, 7, 7, 7] [7, 7, 7])
+                1)
+           (is= (correct-order [] [3])
+                -1)
+           (is= (correct-order [[[]]] [[]])
+                1)
+           (is= (correct-order [1, [2, [3, [4, [5, 6, 7]]]], 8, 9] [1, [2, [3, [4, [5, 6, 0]]]], 8, 9])
+                1))}
+  [a b]
+  (loop [[ca & ra] a
+         [cb & rb] b]
+    (cond
+      ; both are collections
+      (and (coll? ca) (coll? cb))
+      (let [result (correct-order ca cb)]
+        (if (zero? result)
+          (recur ra rb)
+          result))
+
+      ; left is collection and right is not
+      (and (coll? ca) (not (coll? cb)) cb)
+      (let [result (correct-order ca [cb])]
+        (if (zero? result)
+          (recur ra rb)
+          result))
+
+      ; left is not collection and right is
+      (and (not (coll? ca)) (coll? cb) ca)
+      (let [result (correct-order [ca] cb)]
+        (if (zero? result)
+          (recur ra rb)
+          result))
+
+      ; left has values and right doesn't
+      (and ca (not cb)) 1
+
+      (and cb (not ca)) -1
+
+      (and (not ca) (not cb)) 0
+
+      (and (integer? ca) (integer? cb))
+      (cond (< ca cb) -1
+            (= ca cb) (recur ra rb)
+            :else 1)
+
+      :else
+      -1)))
+
+(defn solve-a
+  [input]
+  (->> input
+       (map-indexed (fn [index pairs]
+                      (when (neg? (apply correct-order pairs))
+                        index)))
+       (remove nil?)
+       (map inc)
+       (apply +)))
+
+(deftest description-test-a
+  (is= (solve-a test-input)
+       13))
+
+(defn solve-b
+  [input]
+  (->> (conj input [[[2]] [[6]]])
+       (apply concat)
+       (sort correct-order)
+       (map-indexed (fn [index v] [index v]))
+       (reduce (fn [a [index v]]
+                 (if (or (= v [[2]]) (= v [[6]]))
+                   (conj a (inc index))
+                   a))
+               [])
+       (apply *)))
+
+(deftest description-test-b
+  (is= (solve-b test-input)
+       140))
+
+
+(comment
+
+  (time (solve-a input))
+  ; "Elapsed time: 0.576291 msecs"
+  ; => 5013
+
+  (time (solve-b input))
+  ; "Elapsed time: 1.8127 msecs"
+  ; => 3627
+
+  )
