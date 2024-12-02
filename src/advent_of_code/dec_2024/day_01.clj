20c189038766867b2df28baefffbb3563c54f332
Tomas Ekholm <tomas.ekholm@tietoevry.com>
2024-12-02 12:57:41 +0100

    Added day 1 and 2

---
+++ src/advent_of_code/dec_2024/day_01.clj
@@ -0,0 +1,77 @@
+(ns advent-of-code.dec-2024.day-01
+  (:require [clojure.test :refer [deftest]]
+            [advent-of-code.test :refer [is=]]))
+
+(def input (->> (slurp "src/advent_of_code/dec_2024/day_01_input.txt")
+                (clojure.string/split-lines)))
+
+(def test-input ["3   4"
+                 "4   3"
+                 "2   5"
+                 "1   3"
+                 "3   9"
+                 "3   3"])
+
+(defn total-distance
+  {:test (fn []
+           (is= (total-distance test-input) 11))}
+  [input]
+  (->> input
+       (map (fn [l] (re-seq #"\d+" l)))
+       (reduce (fn [a [le re]]
+                 (-> a
+                     (update :left conj (read-string le))
+                     (update :right conj (read-string re))))
+               {:left  []
+                :right []})
+       (vals)
+       (map sort)
+       (apply map -)
+       (map abs)
+       (reduce +)))
+
+(comment
+  (time (total-distance input))
+  )
+
+; Part Two
+
+(defn similarity-score
+  {:test (fn []
+           (is= (similarity-score test-input) 31))}
+  [input]
+  (let [[first-frequencies second-frequencies] (->> input
+                                                    (map (fn [l] (re-seq #"\d+" l)))
+                                                    (reduce (fn [a [le re]]
+                                                              (-> a
+                                                                  (update :left conj (read-string le))
+                                                                  (update :right conj (read-string re))))
+                                                            {:left  []
+                                                             :right []})
+                                                    (vals)
+                                                    (map frequencies))]
+    (reduce-kv (fn [a value occur-left]
+                 (let [occur-right (get second-frequencies value 0)]
+                   (+ a (* value occur-left occur-right))))
+               0
+               first-frequencies)))
+
+(comment
+  (time (similarity-score input))
+
+  (frequencies "emil tomas aref mattias novin anders")
+  (frequencies [12 12 33 33 4444 44 444 4444 12 12])
+
+  (->> {12 42
+        44 100}
+       (reduce-kv (fn [a k v]
+                    (+ a k v))
+                  0))
+
+  (time (Thread/sleep 1000))
+
+  (def numbers (range 10))
+  (apply + numbers)
+  (reduce + numbers)
+
+  )
---
+++ src/advent_of_code/dec_2024/day_02.clj
@@ -0,0 +1,80 @@
+(ns advent-of-code.dec-2024.day-02
+  (:require [clojure.test :refer [deftest]]
+            [advent-of-code.test :refer [is= is is-not]]))
+
+(def input (->> (slurp "src/advent_of_code/dec_2024/day_02_input.txt")
+                (clojure.string/split-lines)))
+
+(def test-input ["7 6 4 2 1"
+                 "1 2 7 8 9"
+                 "9 7 6 2 1"
+                 "1 3 2 4 5"
+                 "8 6 4 4 1"
+                 "1 3 6 7 9"])
+
+(defn safe?
+  {:test (fn []
+           (is (safe? [7 6 4 2 1]))
+           (is-not (safe? [1 2 7 8 9]))
+           (is-not (safe? [9 7 6 2 1]))
+           (is-not (safe? [1 3 2 4 5]))
+           (is-not (safe? [8 6 4 4 1]))
+           (is (safe? [1 3 6 7 9])))}
+  [report]
+  (and (or (apply < report)
+           (apply > report))
+       (->> report
+            (partition 2 1)
+            (map (fn [p] (abs (apply - p))))
+            (some (fn [x] (> x 3)))
+            (not))))
+
+
+(comment
+  (->> input
+       (map (fn [row] (->> (re-seq #"\d+" row)
+                           (map read-string))))
+       (filter safe?)
+       (count))
+  )
+
+; Part Two
+
+(defn possible-reports
+  {:test (fn []
+           (is= (possible-reports [7 6 4 2 1])
+                [[7 6 4 2 1] [6 4 2 1] [7 4 2 1] [7 6 2 1] [7 6 4 1] [7 6 4 2]]))}
+  [report]
+  (->> (range (count report))
+       (reduce (fn [a i]
+                 (conj a (let [[first-part second-part] (split-at i report)]
+                           (concat first-part (drop 1 second-part)))))
+               [report])))
+
+
+(defn reactor-safe?
+  {:test (fn []
+           (is (reactor-safe? [7 6 4 2 1]))
+           (is-not (reactor-safe? [1 2 7 8 9]))
+           (is-not (reactor-safe? [9 7 6 2 1]))
+           (is (reactor-safe? [1 3 2 4 5]))
+           (is (reactor-safe? [8 6 4 4 1]))
+           (is (reactor-safe? [1 3 6 7 9])))}
+  [report]
+  (->> (possible-reports report)
+       (some safe?)))
+
+(comment
+  (time (->> input
+             (map (fn [row] (->> (re-seq #"\d+" row)
+                                 (map read-string))))
+             (filter reactor-safe?)
+             (count)))
+
+  (->> [1 2 7 8 9 10 11]
+       (partition 2 1))
+
+
+  )
+
+
